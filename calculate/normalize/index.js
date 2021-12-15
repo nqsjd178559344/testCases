b9c56185a32051529e91ece787322df7095bb58b
nqsjd178559344 <1401444513@qq.com>
2021-12-16 00:22:06 +0800

    feat: normalize

---
+++ calculate/normalize/index.js
@@ -0,0 +1,364 @@
+const _data = {
+    "links": [{
+            "source": "发送下单成功短信",
+            "target": "商品物流设置",
+            "label": "<div class=lineLabelContainer><div class=count>57</div><div class=timeSpent>1.00 秒</div></div>",
+            "data": {
+                "count": 57,
+                "timeSpent": 1
+            },
+            "style": "stroke-width: 3.8947368421052633px; fill: none"
+        },
+        {
+            "source": "商品物流设置",
+            "target": "自动分单",
+            "label": "<div class=lineLabelContainer><div class=count>57</div><div class=timeSpent>1.16 秒</div></div>",
+            "data": {
+                "count": 57,
+                "timeSpent": 1.1578947368421053
+            },
+            "style": "stroke-width: 3.8947368421052633px; fill: none"
+        },
+        {
+            "source": "自动分单",
+            "target": "收货地址外呼确认校验",
+            "label": "<div class=lineLabelContainer><div class=count>57</div><div class=timeSpent>13.60 秒</div></div>",
+            "data": {
+                "count": 57,
+                "timeSpent": 13.596491228070175
+            },
+            "style": "stroke-width: 3.8947368421052633px; fill: none"
+        },
+        {
+            "source": "收货地址外呼确认校验",
+            "target": "外呼机器人外呼反馈",
+            "label": "<div class=lineLabelContainer><div class=count>57</div><div class=timeSpent>46 分 12.42 秒</div></div>",
+            "data": {
+                "count": 57,
+                "timeSpent": 2772.4210526315787
+            },
+            "style": "stroke-width: 3.8947368421052633px; fill: none"
+        },
+        {
+            "source": "外呼机器人外呼反馈",
+            "target": "外呼反馈校验",
+            "label": "<div class=lineLabelContainer><div class=count>57</div><div class=timeSpent>1.00 秒</div></div>",
+            "data": {
+                "count": 57,
+                "timeSpent": 1
+            },
+            "style": "stroke-width: 3.8947368421052633px; fill: none"
+        },
+        {
+            "source": "外呼反馈校验",
+            "target": "号码实占",
+            "label": "<div class=lineLabelContainer><div class=count>33</div><div class=timeSpent>1.00 秒</div></div>",
+            "data": {
+                "count": 33,
+                "timeSpent": 1
+            },
+            "style": "stroke-width: 1.7894736842105263px; fill: none"
+        },
+        {
+            "source": "号码实占",
+            "target": "热敏打印",
+            "label": "<div class=lineLabelContainer><div class=count>33</div><div class=timeSpent>30 分 14.55 秒</div></div>",
+            "data": {
+                "count": 33,
+                "timeSpent": 1814.5454545454545
+            },
+            "style": "stroke-width: 1.7894736842105263px; fill: none"
+        },
+        {
+            "source": "热敏打印",
+            "target": "批量出库导入",
+            "label": "<div class=lineLabelContainer><div class=count>33</div><div class=timeSpent>10 分 27.55 秒</div></div>",
+            "data": {
+                "count": 33,
+                "timeSpent": 627.5454545454545
+            },
+            "style": "stroke-width: 1.7894736842105263px; fill: none"
+        },
+        {
+            "source": "批量出库导入",
+            "target": "发送物流短信",
+            "label": "<div class=lineLabelContainer><div class=count>33</div><div class=timeSpent>5.39 秒</div></div>",
+            "data": {
+                "count": 33,
+                "timeSpent": 5.393939393939394
+            },
+            "style": "stroke-width: 1.7894736842105263px; fill: none"
+        },
+        {
+            "source": "发送物流短信",
+            "target": "批量出库导入(无仓位)",
+            "label": "<div class=lineLabelContainer><div class=count>33</div><div class=timeSpent>1.24 秒</div></div>",
+            "data": {
+                "count": 33,
+                "timeSpent": 1.2424242424242424
+            },
+            "style": "stroke-width: 1.7894736842105263px; fill: none"
+        },
+        {
+            "source": "批量出库导入(无仓位)",
+            "target": "微派自动下单",
+            "label": "<div class=lineLabelContainer><div class=count>33</div><div class=timeSpent>9 分 31.97 秒</div></div>",
+            "data": {
+                "count": 33,
+                "timeSpent": 571.969696969697
+            },
+            "style": "stroke-width: 1.7894736842105263px; fill: none"
+        },
+        {
+            "source": "微派自动下单",
+            "target": "已发货T1未签收短信发送",
+            "label": "<div class=lineLabelContainer><div class=count>33</div><div class=timeSpent> 6 小时 17 分</div></div>",
+            "data": {
+                "count": 33,
+                "timeSpent": 22673.757575757576
+            },
+            "style": "stroke-width: 1.7894736842105263px; fill: none"
+        },
+        {
+            "source": "已发货T1未签收短信发送",
+            "target": "派件异常生成外呼单",
+            "label": "<div class=lineLabelContainer><div class=count>33</div><div class=timeSpent> 8 小时 18 分</div></div>",
+            "data": {
+                "count": 33,
+                "timeSpent": 29914.666666666668
+            },
+            "style": "stroke-width: 1.7894736842105263px; fill: none"
+        },
+        {
+            "source": "外呼反馈校验",
+            "target": "订单取消",
+            "label": "<div class=lineLabelContainer><div class=count>24</div><div class=timeSpent>2 天 17 小时</div></div>",
+            "data": {
+                "count": 24,
+                "timeSpent": 234487.375
+            },
+            "style": "stroke-width: 1px; fill: none"
+        }
+    ],
+    "nodes": [{
+            "id": "发送下单成功短信",
+            "label": "<div class=\"labelContainer\"><span class=name>发送下单成功短信 (57)</span></div>",
+            "class": "nodes",
+            "data": {
+                "ins": 0,
+                "outs": 57
+            }
+        },
+        {
+            "id": "商品物流设置",
+            "label": "<div class=\"labelContainer\"><span class=name>商品物流设置 (57)</span></div>",
+            "class": "nodes",
+            "style": "",
+            "data": {
+                "ins": 57,
+                "outs": 57
+            }
+        },
+        {
+            "id": "自动分单",
+            "label": "<div class=\"labelContainer\"><span class=name>自动分单 (57)</span></div>",
+            "class": "nodes",
+            "style": "",
+            "data": {
+                "ins": 57,
+                "outs": 57
+            }
+        },
+        {
+            "id": "收货地址外呼确认校验",
+            "label": "<div class=\"labelContainer\"><span class=name>收货地址外呼确认校验 (57)</span></div>",
+            "class": "nodes",
+            "style": "",
+            "data": {
+                "ins": 57,
+                "outs": 57
+            }
+        },
+        {
+            "id": "外呼机器人外呼反馈",
+            "label": "<div class=\"labelContainer\"><span class=name>外呼机器人外呼反馈 (57)</span></div>",
+            "class": "nodes",
+            "style": "",
+            "data": {
+                "ins": 57,
+                "outs": 57
+            }
+        },
+        {
+            "id": "外呼反馈校验",
+            "label": "<div class=\"labelContainer\"><span class=name>外呼反馈校验 (57)</span></div>",
+            "class": "nodes",
+            "style": "",
+            "data": {
+                "ins": 57,
+                "outs": 57
+            }
+        },
+        {
+            "id": "号码实占",
+            "label": "<div class=\"labelContainer\"><span class=name>号码实占 (33)</span></div>",
+            "class": "nodes",
+            "style": "",
+            "data": {
+                "ins": 33,
+                "outs": 33
+            }
+        },
+        {
+            "id": "热敏打印",
+            "label": "<div class=\"labelContainer\"><span class=name>热敏打印 (33)</span></div>",
+            "class": "nodes",
+            "style": "",
+            "data": {
+                "ins": 33,
+                "outs": 33
+            }
+        },
+        {
+            "id": "批量出库导入",
+            "label": "<div class=\"labelContainer\"><span class=name>批量出库导入 (33)</span></div>",
+            "class": "nodes",
+            "style": "",
+            "data": {
+                "ins": 33,
+                "outs": 33
+            }
+        },
+        {
+            "id": "发送物流短信",
+            "label": "<div class=\"labelContainer\"><span class=name>发送物流短信 (33)</span></div>",
+            "class": "nodes",
+            "style": "",
+            "data": {
+                "ins": 33,
+                "outs": 33
+            }
+        },
+        {
+            "id": "批量出库导入(无仓位)",
+            "label": "<div class=\"labelContainer\"><span class=name>批量出库导入(无仓位) (33)</span></div>",
+            "class": "nodes",
+            "style": "",
+            "data": {
+                "ins": 33,
+                "outs": 33
+            }
+        },
+        {
+            "id": "微派自动下单",
+            "label": "<div class=\"labelContainer\"><span class=name>微派自动下单 (33)</span></div>",
+            "class": "nodes",
+            "style": "",
+            "data": {
+                "ins": 33,
+                "outs": 33
+            }
+        },
+        {
+            "id": "已发货T1未签收短信发送",
+            "label": "<div class=\"labelContainer\"><span class=name>已发货T1未签收短信发送 (33)</span></div>",
+            "class": "nodes",
+            "style": "",
+            "data": {
+                "ins": 33,
+                "outs": 33
+            }
+        },
+        {
+            "id": "派件异常生成外呼单",
+            "label": "<div class=\"labelContainer startNode\"><span class=name>派件异常生成外呼单 (33)</span></div>",
+            "class": "nodes",
+            "style": "",
+            "data": {
+                "ins": 33,
+                "outs": 0
+            }
+        },
+        {
+            "id": "订单取消",
+            "label": "<div class=\"labelContainer startNode\"><span class=name>订单取消 (24)</span></div>",
+            "class": "nodes",
+            "style": "",
+            "data": {
+                "ins": 24,
+                "outs": 0
+            }
+        }
+    ]
+}
+
+const nodes = _data.links.map(item => ({
+    edge: [item.source, item.target],
+    // data:item.data,
+    timeSpent: item.data.timeSpent
+}))
+const links = _data.links.map(item => ({
+    source: item.source,
+    target: item.target,
+    data: item.data,
+}))
+
+console.log(nodes, links)
+
+const _startNodes = links.map(item => item.source);
+const _endNodes = links.map(item => item.target);
+
+const startNodes = _startNodes.filter(item => !_endNodes.includes(item))
+const endNodes = _endNodes.filter(item => !_startNodes.includes(item))
+
+console.log(startNodes, '~~~', endNodes)
+
+const filterNodeAtStart = ['发送下单成功短信']
+const filterNodeAtMiddle = ['自动分单']
+const filterNodeAtEnd = ["订单取消"]
+
+const filterNodes = filterNodeAtStart.concat(filterNodeAtMiddle, filterNodeAtEnd)
+
+const data = normalize(nodes, filterNodes)
+console.log(data, 'all')
+
+function normalize(nodes, unCheckedList) {
+    return nodes.reduce((prev, cur) => {
+        const n1 = cur.edge[0]
+        const n2 = cur.edge[1]
+        // console.log(prev, 'normalize', cur)
+        // todo 是起始节点
+        if (!unCheckedList.includes(n1) && !unCheckedList.includes(n2)) {
+            prev.push(cur)
+            return prev
+        }
+
+        if (startNodes.includes(n1)) {
+            console.log(cur, 'startNodes')
+            return prev
+        }
+        // todo 是结尾节点
+        else if (endNodes.includes(n2)) {
+            console.log(cur, 'endNodes')
+            return prev
+
+            // todo 是中间节点
+        } else {
+            console.log(cur, 'middleNodes')
+            if (unCheckedList.includes(n1)) {
+                const nodeWithTarget = prev.find(item => item.edge[1] === n1)
+                const nodeWithTargetIndex = prev.findIndex(item => item.edge[1] === n1)
+                console.log(cur, '修改结尾', nodeWithTarget)
+                const newEdg = {
+                    edge: [nodeWithTarget.edge[0], cur.edge[1]],
+                    timeSpent:nodeWithTarget.timeSpent + cur.timeSpent
+                }
+                prev[nodeWithTargetIndex] = newEdg
+                return prev
+            }
+            prev.push(cur)
+            return prev
+        }
+
+    }, [])
+}
